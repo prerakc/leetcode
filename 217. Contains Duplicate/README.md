ece2087df47bdfc1fa9c298a131a6ffb287cd9a2
prerakc <prerak.chaudhari@yahoo.ca>
2022-06-03 17:40:29 -0400

    217. Contains Duplicate

---
+++ 217. Contains Duplicate/README.md
@@ -0,0 +1,11 @@
+# 217. Contains Duplicate
+
+## Link
+https://leetcode.com/problems/contains-duplicate/
+
+## Solution
+Use a hashset to keep track of visited elements in the list. While iterating, check if the current element is in the set.
+
+Time complexity: O(n)
+
+Space complexity: O(n)
---
+++ 217. Contains Duplicate/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        
+        for n in nums:
+            if n in s:
+                return True
+            else:
+                s.add(n)
+                
+        return False
